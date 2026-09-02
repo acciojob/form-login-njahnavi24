e68d8ffadd17b703251b1ef513588a11731a0067
njahnavi24 <jahnavinagavalli24@gmail.com>
2026-09-02 17:22:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
+	let fname = document.querySelector('[name = "fname"]').value.trim()
+	let lname = document.querySelector('[name = "lname"]').value.trim()
 
+	alert(fname + " " + lname)
 }
